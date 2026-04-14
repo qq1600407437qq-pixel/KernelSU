ae58904eb7eee2fbecb123b45f4310fb86ac3efd
qq1600407437qq-pixel <qq1600407437qq@gmail.com>
2026-04-14 12:04:09 +0800

    Delete _worker.js

--- _worker.js
+++
@@ -1,9 +0,0 @@
-export default {
-  async fetch(request, env, ctx) {
-    const url = "https://raw.githubusercontent.com/cmliu/edgetunnel/refs/heads/main/_worker.js";
-    const res = await fetch(url);
-    const code = await res.text();
-    const module = await import("data:text/javascript," + encodeURIComponent(code));
-    return module.default.fetch(request, env, ctx);
-  }
-};
